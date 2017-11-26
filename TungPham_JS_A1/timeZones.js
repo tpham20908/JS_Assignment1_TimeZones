9a9a30efecc9ce6e254362c32b29e17eab71fca3
TP <tungint@gmail.com>
2017-11-25 22:10:03 -0500

    complete assignment1

---
+++ TungPham_JS_A1/timeZones.js
@@ -0,0 +1,45 @@
+/*
+   JavaScript & AJAX Assignment 01
+   Developed By: Thanh-Tung Pham
+   Developed Date: Nov 25, 2017
+
+   Function List:
+   addTime(oldTime, milliseconds)
+      Used to add a specified number of milliseconds to a date object named oldtime.
+      A new date object with the new time value is returned by the function.
+
+   showTime(time)
+      Displays a time value in the format:
+      hh:mm AM/PM
+*/
+
+/**
+ *
+ * @param {any} oldTime
+ * @param {any} milliseconds
+ */
+function addTime(oldTime, milliseconds) {
+    // Declare and Initialize variables
+    var newTime = new Date();
+    var newValue = oldTime.getTime() + milliseconds;
+    // Processing
+    newTime.setTime(newValue);
+
+    return newTime;
+}
+
+/**
+ * 
+ * @param {any} time
+ */
+function showTime(time) {
+    var hour = time.getHours();
+    var minute = time.getMinutes();
+    var hourDisplay = (hour > 12) ? hour - 12 : hour;
+    var minuteDisplay = (minute < 10) ? "0" + minute : "" + minute;
+    var timeDisplay = hourDisplay + ":" + minuteDisplay;
+
+    timeDisplay += (hour >= 12) ? " PM" : " AM";
+
+    return timeDisplay;
+}
---
+++ TungPham_JS_A1/timeZonestxt.js
@@ -0,0 +1,19 @@
+/*
+   JavaScript & AJAX Assignment 01 
+   Developed By: 
+   Developed Date:   
+
+   Function List:
+   addTime(oldtime, milliseconds)
+      Used to add a specified number of milliseconds to a date object named oldtime.
+      A new date object with the new time value is returned by the function.
+
+   showTime(time)
+      Displays a time value in the format:
+      hh:mm AM/PM
+*/
+
+
+
+
+
